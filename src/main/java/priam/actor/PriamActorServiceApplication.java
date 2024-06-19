825816258cc099ee5999fe9f5b6f8eb7e79e7a63
Thumus <thumus@tutanota.com>
2024-06-19 11:42:04 +0200

    add: keycloak(lamari)

--- src/main/java/priam/actor/PriamActorServiceApplication.java
+++ src/main/java/priam/actor/PriamActorServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import priam.actor.repositories.DataSubjectCategoryRepository;
 import priam.actor.repositories.DataSubjectRepository;
 
 @SpringBootApplication
+@Configuration
 public class PriamActorServiceApplication {
 
     public static void main(String[] args) {
